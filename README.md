3f8db225d9bd777689c1f7d7d5549fe7e78e9105
Chris Eppstein <chris@eppsteins.net>
2015-05-08 10:50:13 -0700

    Initial implementation

---
+++ README.md
@@ -0,0 +1,106 @@
+# node-sass-utils
+
+This package provides helpers for working with the Sass values that `node-sass`
+passes to javascript functions that are exposed as sass functions.
+
+## Basic Usage
+
+```js
+var sass = require("node-sass");
+var sassUtils = require("node-sass-utils");
+
+// standard invocation: methods on the sassUtils object
+function mySassFunction(arg1) {
+  console.log(sassUtils.sassString(arg1));
+  return arg1;
+}
+
+```
+
+The `sassUtils` object provides many useful helper methods.
+
+Many of the Sass Utility functions are more naturally instance methods
+of the sass types. The sass utilities can infect the Sass types.
+
+
+```js
+var sass = require("node-sass");
+var sassUtils = require("node-sass-utils");
+
+sassUtils.infect(sass.types);
+
+function mySassFunction(arg1) {
+  console.log(arg1.sassString());
+  return arg1;
+}
+```
+
+However library authors really should not mess around with core objects.
+If you really need to, you can disinfect the sass types:
+
+```js
+var sass = require("node-sass");
+var sassUtils = require("node-sass-utils");
+
+function mySassFunction(arg1) {
+  sassUtils.infect(sass.types);
+  console.log(arg1.sassString());
+  sassUtils.disinfect(sass.types);
+  return arg1;
+}
+```
+
+## API
+
+### `sassUtils.assertType(value, typeName)`
+
+Raises an error unless the sass value is not of the type specified. The
+type should be a javascript string as would be returned by `sassUtils.typeOf`.
+
+### `sassUtils.isFalsy(value)`
+
+Returns true if the value is a sass null or boolean false.
+
+### `sassUtils.isNull(value)`
+
+Returns true if the value is a sass null.
+
+### `sassUtils.sassString(value)`
+
+Returns a javascript string that is the sass value as represented in a
+Sass file.
+
+### `sassUtils.typeOf(value)`
+
+Returns a javascript string that is the sass type name of value (as
+returned by the sass `type-of`. One of the following: `"bool"`,
+`"color"`, `"list"`, `"map"`, `"null"`, `"number"`, `"string"`
+
+### `sassUtils.typeOfConstructor(constructor)`
+
+Like `sassUtils.typeOf` except it takes a sass value constructor
+(`sass.types.Xyz` or `sassValue.constructor`) instead of a sass value itself.
+
+### `sassUtils.unquote(value)`
+
+Returns a new sass string after unquoting it. Returns a sass null if passed
+a sass null. All other types raise an error.
+
+## Instance API
+
+These methods are only available after calling
+`sassUtils.infect(sass.types)`.
+
+### `isFalsy()`
+
+Returns true if the instance is a sass null or boolean false.
+
+### `isNull()`
+
+Returns true if the instance is a sass null.
+
+### `sassString()`
+
+Returns a javascript string that is the instance as represented in a
+Sass file.
+
---
+++ test/test_sass_utils.js
@@ -0,0 +1,127 @@
+"use strict";
+
+var assert = require("assert");
+var sass = require("node-sass");
+var sassUtils = require("../lib")(sass);
+
+describe("sass utils clean", function () {
+  it("should stringify null", function (done) {
+    assert.equal("null", sassUtils.sassString(sass.types.Null()));
+    done();
+  });
+  it("should stringify true", function (done) {
+    assert.equal("true", sassUtils.sassString(sass.types.Boolean(true)));
+    done();
+  });
+  it("should stringify false", function (done) {
+    assert.equal("false", sassUtils.sassString(sass.types.Boolean(false)));
+    done();
+  });
+  it("should stringify numbers", function (done) {
+    assert.equal("10", sassUtils.sassString(sass.types.Number(10)));
+    done();
+  });
+  it("should stringify numbers with units", function (done) {
+    assert.equal("10px", sassUtils.sassString(sass.types.Number(10, "px")));
+    done();
+  });
+  it("should stringify strings", function (done) {
+    assert.equal("foo", sassUtils.sassString(sass.types.String("foo")));
+    done();
+  });
+  it("should stringify quoted strings", function (done) {
+    assert.equal('"foo"', sassUtils.sassString(sass.types.String('"foo"')));
+    done();
+  });
+  it("should stringify empty lists", function (done) {
+    var l = sass.types.List(0);
+    assert.equal("()", sassUtils.sassString(l));
+    done();
+  });
+  it("should stringify lists of 1 element", function (done) {
+    var l = sass.types.List(1);
+    l.setValue(0, sass.types.Number(5, "px"));
+    assert.equal("(5px,)", sassUtils.sassString(l));
+    done();
+  });
+  it("should stringify lists", function (done) {
+    var l = sass.types.List(3);
+    l.setValue(0, sass.types.Number(5, "px"));
+    l.setValue(1, sass.types.Boolean(true));
+    l.setValue(2, sass.types.String("'asdf'"));
+    assert.equal("(5px, true, 'asdf')", sassUtils.sassString(l));
+    done();
+  });
+  it("should stringify space delimited lists", function (done) {
+    var l = sass.types.List(3);
+    l.setSeparator(false);
+    l.setValue(0, sass.types.Number(5, "px"));
+    l.setValue(1, sass.types.Boolean(true));
+    l.setValue(2, sass.types.String("'asdf'"));
+    assert.equal("(5px true 'asdf')", sassUtils.sassString(l));
+    done();
+  });
+  it("should stringify empty maps", function (done) {
+    var m = sass.types.Map(0);
+    assert.equal("()", sassUtils.sassString(m));
+    done();
+  });
+  it("should stringify maps", function (done) {
+    var m = sass.types.Map(1);
+    m.setKey(0, sass.types.Number(2, "px"));
+    m.setValue(0, sass.types.Color(30, 127, 0, 1.0));
+    assert.equal("(2px: rgba(30, 127, 0, 1))", sassUtils.sassString(m));
+    done();
+  });
+  it("null unquotes as null", function(done) {
+    var n = sass.types.Null();
+    assert.equal("null", sassUtils.typeOf(n));
+    assert.equal("null", sassUtils.typeOf(sassUtils.unquote(n)));
+    done();
+  });
+  it("knows simplified type names (as returned by sass's type-of function)", function(done) {
+    assert.equal("null", sassUtils.typeOf(sass.types.Null()));
+    assert.equal("bool", sassUtils.typeOf(sass.types.Boolean(true)));
+    assert.equal("number", sassUtils.typeOf(sass.types.Number(2, "px")));
+    assert.equal("string", sassUtils.typeOf(sass.types.String("foo")));
+    assert.equal("list", sassUtils.typeOf(sass.types.List(0)));
+    assert.equal("map", sassUtils.typeOf(sass.types.Map(0)));
+    done();
+  });
+  it("null unquotes as null", function(done) {
+    var n = sass.types.Null();
+    assert.equal("null", sassUtils.typeOf(n));
+    assert.equal("null", sassUtils.typeOf(sassUtils.unquote(n)));
+    done();
+  });
+  it("can check for null", function(done) {
+    assert.equal(true, sassUtils.isNull(sass.types.Null()));
+    assert.equal(false, sassUtils.isNull(sass.types.Boolean(true)));
+    assert.equal(false, sassUtils.isNull(sass.types.Number(2, "px")));
+    assert.equal(false, sassUtils.isNull(sass.types.String("foo")));
+    assert.equal(false, sassUtils.isNull(sass.types.List(0)));
+    assert.equal(false, sassUtils.isNull(sass.types.Map(0)));
+    done();
+  });
+  it("can check for falsy values", function(done) {
+    assert.equal(true, sassUtils.isFalsy(sass.types.Null()));
+    assert.equal(true, sassUtils.isFalsy(sass.types.Boolean(false)));
+    assert.equal(false, sassUtils.isFalsy(sass.types.Boolean(true)));
+    assert.equal(false, sassUtils.isFalsy(sass.types.Number(0)));
+    assert.equal(false, sassUtils.isFalsy(sass.types.String("")));
+    assert.equal(false, sassUtils.isFalsy(sass.types.List(0)));
+    assert.equal(false, sassUtils.isFalsy(sass.types.Map(0)));
+    done();
+  });
+  // it("can convert to and from js maps", function(done) {
+  //   var map = new Map();
+  //   map.set(sass.types.String("foo"), sass.types.Color(0, 0, 0));
+  //   map.set(sass.types.String("bar"), sass.types.Number(2, "px"));
+  //   var sassMap = sassUtils.fromJSMap(map);
+  //   assert.equal("foo", sassMap.getKey(0).getValue());
+  //   assert.equal("bar", sassMap.getKey(1).getValue());
+  //   var newMap = sassUtils.toJSMap(sassMap);
+  //   assert.equal(2, newMap.get(sass.types.String("bar")).getValue());
+  //   done();
+  // });
+});
