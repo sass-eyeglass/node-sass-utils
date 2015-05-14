d415154def7690e29a7c703c680e3ce7251809fb
Chris Eppstein <chris@eppsteins.net>
2015-05-13 18:50:24 -0700

    Sass Maps that adhere to the JS Map API.

--- README.md
+++ README.md
@@ -86,11 +86,55 @@ Like `sassUtils.typeOf` except it takes a sass value constructor
 Returns a new sass string after unquoting it. Returns a sass null if passed
 a sass null. All other types raise an error.
 
-### `new sassUtils.SassMap()`
+### `new sassUtils.SassMap([sassMap])`
 
 Returns a new javascript Map that is capable of maping sass objects as
 keys to sass objects as values. The API of a sass map is identical to the
 [Javascript Map][map_type] builtin type.
+
+Additional Methods:
+
+* `map.update(otherMap[, conflictHandler])` - adds the keys and values from `otherMap` into
+  `map`. If the same key is present in both maps, the value from `otherMap`
+  is used. `conflictHandler` is a function that accepts three arguments:
+  `key`, `newValue` (from `otherMap`), and `oldValue` (from `map`). The
+  function should return a value to be set into `map` for `key`. E.g. It
+  could always pick the `oldValue`, or it could pick the maximum, or
+  create an average, etc. `otherMap` can be a standard SassMap or a
+  SassJsMap.
+
+* `map.deepUpdate(otherMap[, conflictHandler])` - like `update` except when
+  the values for the same key are both maps, the update recurses into
+  those maps. Values that are Sass maps will be converted to SassJsMap instances if
+  they are updated with another map.
+
+* `map.toSassMap()` - converts a SassJsMap to a SassMap suitable for
+  returning from a custom js function. If any value in the map is a SassJsMap it
+  will also be converted.
+
+* `map.sassString()` - returns a string for this map as it would be
+  typed in a sass file.
+
+* `map.typeOf()` - returns `"map"`.
+
+**Note:** It is highly discouraged to use SassJsMap values as a key in
+ another map. The behavior is very unpredictable if the map is mutated.
+
+Usage Example:
+
+```
+var sass = require("node-sass");
+var someSassColorLibrary = require("some-color-library");
+var sassUtils = require("node-sass-utils")(sass);
+function someCustomSassFunction(sassMap) {
+  var map = new sassUtils.SassJsMap();
+  var color = map.get(sass.types.String("color"));
+  map.set(sass.types.String("background-color"),
+          someSassColorLibrary.darken(color, 0.20))
+  return map.toSassMap();
+}
+```
+
 
 ## Instance API
 
---
+++ test/test_sass_js_map.js
@@ -0,0 +1,179 @@
+"use strict";
+
+var assert = require("assert");
+var sass = require("node-sass");
+var sassUtils = require("../lib")(sass);
+
+describe("sass js map", function () {
+  it("can be constructed", function (done) {
+    var map = new sassUtils.SassJsMap();
+    assert(map);
+    done();
+  });
+
+  it("can be constructed from a Sass map", function (done) {
+    var sassMap = sass.types.Map(2);
+    sassMap.setKey(0, sass.types.String("a"));
+    sassMap.setValue(0, sass.types.Number(1));
+    sassMap.setKey(1, sass.types.String("b"));
+    sassMap.setValue(1, sass.types.Number(2));
+    var map = new sassUtils.SassJsMap(sassMap);
+    assert.equal(map.size, 2);
+    assert.equal(map.get(sass.types.String("a")).getValue(), 1);
+    assert.equal(map.get(sass.types.String("b")).getValue(), 2);
+    done();
+  });
+
+  it("doesn't allow illegal keys", function(done) {
+    try {
+      var map = new sassUtils.SassJsMap();
+      map.set("hi", sass.types.String("bye"));
+    } catch (e) {
+      assert.equal(e.message, 'Not a legal Sass value: "hi"');
+      done();
+      return;
+    }
+    assert(false, "did not raise");
+  });
+
+  it("doesn't allow illegal values", function(done) {
+    try {
+      var map = new sassUtils.SassJsMap();
+      map.set(sass.types.String("hi"), "bye");
+    } catch (e) {
+      assert.equal(e.message, 'Not a legal Sass value: "bye"');
+      done();
+      return;
+    }
+    assert(false, "did not raise");
+  });
+
+  it("has a length", function (done) {
+    var map = new sassUtils.SassJsMap();
+    assert.equal(0, map.size);
+    done();
+  });
+
+  it("can map sass value to other sass values", function (done) {
+    var map = new sassUtils.SassJsMap();
+    var value = sass.types.String("hi");
+    map.set(sass.types.Number(2, "px"), value);
+    assert.equal(value, map.get(sass.types.Number(2, "px")));
+    assert.equal(1, map.size);
+    done();
+  });
+
+  it("can clear", function (done) {
+    var map = new sassUtils.SassJsMap();
+    map.set(sass.types.Number(2, "px"), sass.types.String("hi"));
+    assert.equal(1, map.size);
+    map.clear();
+    assert.equal(0, map.size);
+    done();
+  });
+
+  it("can remove keys", function (done) {
+    var map = new sassUtils.SassJsMap();
+    assert.equal(false, map.delete(sass.types.Number(2, "px")));
+    map.set(sass.types.Number(2, "px"), sass.types.String("hi"));
+    assert.equal(1, map.size);
+    assert.equal(true, map.delete(sass.types.Number(2, "px")));
+    assert.equal(0, map.size);
+    assert.equal(undefined, map.get(sass.types.Number(2, "px")));
+    done();
+  });
+  it("can check for a key", function(done) {
+    var map = new sassUtils.SassJsMap();
+    assert.equal(false, map.has(sass.types.Number(2, "px")));
+    map.set(sass.types.Number(2, "px"), sass.types.String("hi"));
+    assert.equal(true, map.has(sass.types.Number(2, "px")));
+    done();
+  });
+  it("has entries", function(done) {
+    var map = new sassUtils.SassJsMap();
+    var k = sass.types.Number(2, "px");
+    var v = sass.types.String("hi");
+    map.set(k, v);
+    var entryIterator = map.entries();
+    var pair = entryIterator.next().value;
+    assert.equal(k, pair[0]);
+    assert.equal(v, pair[1]);
+    assert(entryIterator.next().done);
+    done();
+  });
+  it("has keys", function(done) {
+    var map = new sassUtils.SassJsMap();
+    var k = sass.types.Number(2, "px");
+    var v = sass.types.String("hi");
+    map.set(k, v);
+    var iterator = map.keys();
+    var key = iterator.next().value;
+    assert.equal(k, key);
+    assert(iterator.next().done);
+    done();
+  });
+  it("has values", function(done) {
+    var map = new sassUtils.SassJsMap();
+    var k = sass.types.Number(2, "px");
+    var v = sass.types.String("hi");
+    map.set(k, v);
+    var iterator = map.values();
+    var value = iterator.next().value;
+    assert.equal(v, value);
+    assert(iterator.next().done);
+    done();
+  });
+  it("can be iterated with forEach", function(done) {
+    var map = new sassUtils.SassJsMap();
+    var k = sass.types.Number(2, "px");
+    var v = sass.types.String("hi");
+    map.set(k, v);
+    map.forEach(function(value, key, m) {
+      assert.equal(k, key);
+      assert.equal(v, value);
+      assert.equal(map, m);
+    });
+    done();
+  });
+  it("can update from a sass js map", function(done) {
+    var map = new sassUtils.SassJsMap();
+    var k = sass.types.String("hi");
+    var v = sass.types.Number(2, "px");
+    map.set(k, v);
+    var anotherMap = new sassUtils.SassJsMap();
+    anotherMap.update(map);
+    var iterator = anotherMap.values();
+    var value = iterator.next().value;
+    assert.equal(v, value);
+    assert(iterator.next().done);
+    done();
+  });
+
+  it("can deep update a map", function(done) {
+    var map1 = new sassUtils.SassJsMap();
+    var k = sass.types.String("top-level");
+    var v = sass.types.Map(1);
+    v.setKey(0, sass.types.String("a"));
+    v.setValue(0, sass.types.Number(1));
+    map1.set(k, v);
+
+    var map2 = new sassUtils.SassJsMap();
+    var k2 = sass.types.String("top-level");
+    var v2 = sass.types.Map(2);
+    v2.setKey(0, sass.types.String("a"));
+    v2.setValue(0, sass.types.Number(2));
+    v2.setKey(1, sass.types.String("b"));
+    v2.setValue(1, sass.types.Number(3));
+    map2.set(k2, v2);
+
+    map1.deepUpdate(map2);
+    var submap = map1.get(sass.types.String("top-level"));
+    var replaced = submap.get(sass.types.String("a"));
+    var added = submap.get(sass.types.String("b"));
+    assert.equal(replaced.getValue(), 2);
+    assert.equal(added.getValue(), 3);
+    assert.equal(map1.sassString(), "(top-level: (a: 2, b: 3))");
+    assert.equal(sassUtils.sassString(map1.toSassMap()), "(top-level: (a: 2, b: 3))");
+    done();
+  });
+});
